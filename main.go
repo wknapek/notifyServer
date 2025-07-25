ecf9e7c029081561ddfb09e11aabbb17ab12191d
witoldknapek <wknapek@gmail.com>
2025-07-25 21:00:23 +0200

    add counter

--- main.go
+++ main.go
@@ -6,17 +6,27 @@ import (
 	"net/http"
 )
 
+var msgCounter int64
+
 func log(w http.ResponseWriter, req *http.Request) {
 	body, err := io.ReadAll(req.Body)
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(string(body))
-	req.Body.Close()
+	msgCounter++
+	fmt.Println("received messages:", msgCounter)
+	err = req.Body.Close()
+	if err != nil {
+		return
+	}
 	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
 	http.HandleFunc("/hello", log)
-	http.ListenAndServe(":8090", nil)
+	err := http.ListenAndServe(":8090", nil)
+	if err != nil {
+		return
+	}
 }
